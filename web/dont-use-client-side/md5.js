5b94f0a58f36d8cdc00d1673a0d76cfa9166e05f
vvbjorn <119787991+vvbjorn@users.noreply.github.com>
2025-09-14 15:25:26 +0200

    fix

--- web/dont-use-client-side/md5.js
+++ web/dont-use-client-side/md5.js
@@ -26,17 +26,17 @@
     
   }
 
-    if (checkpass.substring(0, split) == 'pico') {
+//     if (checkpass.substring(0, split) == 'pico') {
 
-        if (checkpass.substring(split, split*2) == 'CTF{') {
+//         if (checkpass.substring(split, split*2) == 'CTF{') {
 
-              if (checkpass.substring(split*2, split*3) == 'no_c') {
+//               if (checkpass.substring(split*2, split*3) == 'no_c') {
 
-          if (checkpass.substring(split*3, split*4) == 'lien') {
+//           if (checkpass.substring(split*3, split*4) == 'lien') {
 
-         if (checkpass.substring(split*4, split*5) == 'ts_p') {
-            if (checkpass.substring(split*5, split*6) == 'lz_7') {
-      if (checkpass.substring(split*6, split*7) == '723c') {
-                if (checkpass.substring(split*7, split*8) == 'e}') {
+//          if (checkpass.substring(split*4, split*5) == 'ts_p') {
+//             if (checkpass.substring(split*5, split*6) == 'lz_7') {
+//       if (checkpass.substring(split*6, split*7) == '723c') {
+//                 if (checkpass.substring(split*7, split*8) == 'e}') {
 
-picoCTF{no_clients_plz_7723ce}
+// picoCTF{no_clients_plz_7723ce}
